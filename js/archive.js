915ce6d9f533cfb93de24ca2f4b77d71c5900935
Simning <120423958+Simning@users.noreply.github.com>
2024-05-13 18:05:15 +0900

    js header 수정 완료 및 다크모드 로직 수정 완료

--- js/archive.js
+++ js/archive.js
@@ -2,26 +2,33 @@ class TabManager {
   constructor() {
     this.tabs = document.querySelectorAll('.tab');
     this.items = document.querySelectorAll('.item');
-
     this.tabs.forEach(tab => {
       tab.addEventListener('click', () => this.toggleTab(tab));
     });
   }
 
+  // 클릭된 탭을 받아 해당 탭을 활성화하고, 관련된 항목을 표시
   toggleTab(clickedTab) {
+    // 현재 활성화된 탭을 비활성화
     document.querySelector('.tab.active').classList.remove('active');
+    // 클릭된 탭을 활성화
     clickedTab.classList.add('active');
 
+    // 모든 항목을 숨김
     this.items.forEach(item => {
       item.style.display = 'none';
     });
 
+    // 클릭된 탭의 텍스트를 대문자로 변환하여 카테고리를 가져오기
     const category = clickedTab.textContent.toUpperCase();
+    // 만약 카테고리가 'ALL'이면, 모든 항목을 표시
     if (category === 'ALL') {
       this.items.forEach(item => {
         item.style.display = 'block';
       });
     } else {
+      // 그렇지 않으면, 각 항목의 레이블 텍스트를 가져와서 대문자로 변환
+      // 해당 카테고리를 포함하는 경우 해당 항목을 표시
       this.items.forEach(item => {
         const label = item.querySelector('.label').textContent.toUpperCase();
         if (label.includes(category)) {
@@ -32,5 +39,5 @@ class TabManager {
   }
 }
 
-new TabManager(); // 탭 관리기능을 초기화합니다.
+new TabManager();
 export default TabManager;
--- js/header.js
+++ js/header.js
@@ -25,45 +25,54 @@ class HeaderManager {
 
   /////* 페이지 내비게이션을 설정 */////
   setupNavigation() {
-    const currentPath = window.location.pathname;
-    const currentFileName = currentPath.substring(currentPath.lastIndexOf('/') + 1);
-    console.log(`currentFileName = ${currentFileName}`);
-  
-    document.querySelectorAll('.navLink').forEach(link => {
-      const linkPath = link.getAttribute('href').replace('./', '/');
-      const linkFileName = linkPath.substring(linkPath.lastIndexOf('/') + 1);
-      console.log(`linkFileName = ${linkFileName}`);
-  
-      if (linkFileName === currentFileName || (linkFileName === '' && currentFileName === 'index.html')) {
-        link.classList.add('active');
-      } else {
-        link.classList.remove('active');
-      }
-    });
+  const currentPath = window.location.pathname;
+  // 현재 페이지의 경로에서 마지막 슬래시('/') 뒤에 있는 파일명을 추출하여 저장
+  const currentFileName = currentPath.substring(currentPath.lastIndexOf('/') + 1);
+
+  document.querySelectorAll('.navLink').forEach(link => {
+    // 각 링크의 href 속성 값을 가져와서 './'을 '/'로 대체하여 실제 경로를 가져오기
+    const linkPath = link.getAttribute('href').replace('./', '/');
+    // 각 링크의 경로에서 마지막 슬래시('/') 뒤에 있는 파일명을 추출하여 저장
+    const linkFileName = linkPath.substring(linkPath.lastIndexOf('/') + 1);
+
+    // 현재 페이지의 파일명과 링크의 파일명을 비교하여 조건문 실행
+    if (linkFileName === currentFileName || (linkFileName === '' && currentFileName === 'index.html')) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
   }
 
   /////* 다크 모드 설정 기능을 초기화 */////
   setupDarkMode() {
     const modeWrap = document.querySelector('.modeWrap');
     if (!modeWrap) return; // 요소가 존재하지 않으면 함수 종료
-
+    
     const lightModeBtn = modeWrap.querySelector('.light');
     const darkModeBtn = modeWrap.querySelector('.dark');
-
+    
+    // 사용자의 시스템 설정에 따라 다크 모드 버튼을 자동으로 활성화 또는 비활성화
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      darkModeBtn.classList.add('active'); // 사용자가 다크 모드로 설정한 경우 darkMode 버튼 활성화
+    } else {
+      lightModeBtn.classList.add('active'); // 기본 모드로 설정한 경우 lightMode 버튼 활성화
+    }
+    
     // 밝은 모드 버튼 클릭 시 이벤트 처리
     lightModeBtn.addEventListener('click', e => {
       e.preventDefault();
       lightModeBtn.classList.add('active'); // 밝은 모드 버튼에 활성 클래스 추가
       darkModeBtn.classList.remove('active'); // 어두운 모드 버튼의 활성 클래스 제거
-      // 여기에 다크모드 해제 로직 추가
+      document.body.classList.remove('dark-mode'); // 다크 모드 클래스 제거
     });
-
+    
     // 어두운 모드 버튼 클릭 시 이벤트 처리
     darkModeBtn.addEventListener('click', e => {
       e.preventDefault();
       darkModeBtn.classList.add('active'); // 어두운 모드 버튼에 활성 클래스 추가
       lightModeBtn.classList.remove('active'); // 밝은 모드 버튼의 활성 클래스 제거
-      // 여기에 다크모드 활성화 로직 추가
+      document.body.classList.add('dark-mode'); // 다크 모드 클래스 추가
     });
   }
 
