b73fd7b10562fae7840110313b36eef9d62911df
Simning <120423958+Simning@users.noreply.github.com>
2024-06-21 17:52:31 +0900

    240621 commit

--- js/header.js
+++ js/header.js
@@ -1,94 +1,68 @@
 class HeaderManager {
   constructor() {
+    this.modeWraps = document.querySelectorAll('.modeWrap');
+    this.lightModeBtns = Array.from(this.modeWraps).map(modeWrap => modeWrap.querySelector('.light'));
+    this.darkModeBtns = Array.from(this.modeWraps).map(modeWrap => modeWrap.querySelector('.dark'));
+
     this.setupNavigation();
     this.setupDarkMode();
     this.setupHeaderScroll();
   }
 
-  /////* 페이지 내비게이션을 설정 */////
+  // 내비게이션 설정
   setupNavigation() {
-    const currentPath = window.location.pathname;
-    // 현재 페이지의 경로에서 마지막 슬래시('/') 뒤에 있는 파일명을 추출하여 저장
-    const currentFileName = currentPath.substring(currentPath.lastIndexOf('/') + 1);
+    const currentFileName = window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1);
 
     document.querySelectorAll('.navLink').forEach(link => {
-      // 각 링크의 href 속성 값을 가져와서 './'을 '/'로 대체하여 실제 경로를 가져오기
       const linkPath = link.getAttribute('href').replace('./', '/');
-      // 각 링크의 경로에서 마지막 슬래시('/') 뒤에 있는 파일명을 추출하여 저장
       const linkFileName = linkPath.substring(linkPath.lastIndexOf('/') + 1);
+      const isActive = linkFileName === currentFileName || (linkFileName === '' && currentFileName === 'index.html') || (link.textContent.trim().toLowerCase() === 'projects' && (currentFileName === 'projects.html' || currentFileName.startsWith('projView')));
 
-      // 현재 페이지의 파일명과 링크의 파일명을 비교하여 조건문 실행
-      if (
-        linkFileName === currentFileName || 
-        (linkFileName === '' && currentFileName === 'index.html') || 
-        (link.textContent.trim().toLowerCase() === 'projects' && (currentFileName === 'projects.html' || currentFileName.startsWith('projView')))
-      ) {
-        link.classList.add('active');
-      } else {
-        link.classList.remove('active');
-      }
+      link.classList.toggle('active', isActive);
     });
   }
 
-  /////* 다크 모드 설정 기능을 초기화 */////
+  // 다크 모드 설정
   setupDarkMode() {
-    const modeWrap = document.querySelector('.modeWrap');
-    if (!modeWrap) return; // 요소가 존재하지 않으면 함수 종료
-    
-    const lightModeBtn = modeWrap.querySelector('.light');
-    const darkModeBtn = modeWrap.querySelector('.dark');
+    if (!this.modeWraps.length) return;
 
-    // 로컬 스토리지에서 저장된 모드 가져오기
-    const savedMode = localStorage.getItem('color-mode');
+    const savedMode = localStorage.getItem('color-mode') || (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+    this.setMode(savedMode);
     
-    if (savedMode) {
-      if (savedMode === 'dark') {
-        darkModeBtn.classList.add('active');
-        document.body.classList.add('dark-mode');
-      } else {
-        lightModeBtn.classList.add('active');
-        document.body.classList.remove('dark-mode');
-      }
+    this.lightModeBtns.forEach(btn => btn.addEventListener('click', () => this.setMode('light')));
+    this.darkModeBtns.forEach(btn => btn.addEventListener('click', () => this.setMode('dark')));
+  }
+
+  // 모드 설정
+  setMode(mode) {
+    const isDarkMode = mode === 'dark';
+
+    if (isDarkMode) {
+      this.darkModeBtns.forEach(btn => btn.classList.add('active'));
+      this.lightModeBtns.forEach(btn => btn.classList.remove('active'));
+      document.body.classList.add('dark-mode');
     } else {
-      // 사용자의 시스템 설정에 따라 초기 모드 설정
-      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        darkModeBtn.classList.add('active'); // 사용자가 다크 모드로 설정한 경우 darkMode 버튼 활성화
-        document.body.classList.add('dark-mode');
-      } else {
-        lightModeBtn.classList.add('active'); // 기본 모드로 설정한 경우 lightMode 버튼 활성화
-        document.body.classList.remove('dark-mode');
-      }
+      this.lightModeBtns.forEach(btn => btn.classList.add('active'));
+      this.darkModeBtns.forEach(btn => btn.classList.remove('active'));
+      document.body.classList.remove('dark-mode');
     }
 
-    // 밝은 모드 버튼 클릭 시 이벤트 처리
-    lightModeBtn.addEventListener('click', e => {
-      lightModeBtn.classList.add('active'); // 밝은 모드 버튼에 활성 클래스 추가
-      darkModeBtn.classList.remove('active'); // 어두운 모드 버튼의 활성 클래스 제거
-      document.body.classList.remove('dark-mode'); // 다크 모드 클래스 제거
-      localStorage.setItem('color-mode', 'light'); // 로컬 스토리지에 밝은 모드 저장
-    });
-    
-    // 어두운 모드 버튼 클릭 시 이벤트 처리
-    darkModeBtn.addEventListener('click', e => {
-      darkModeBtn.classList.add('active'); // 어두운 모드 버튼에 활성 클래스 추가
-      lightModeBtn.classList.remove('active'); // 밝은 모드 버튼의 활성 클래스 제거
-      document.body.classList.add('dark-mode'); // 다크 모드 클래스 추가
-      localStorage.setItem('color-mode', 'dark'); // 로컬 스토리지에 어두운 모드 저장
-    });
+    localStorage.setItem('color-mode', mode);
   }
 
-  /////* 헤더 스크롤 제어 기능을 초기화 */////
+  // 헤더 스크롤 제어
   setupHeaderScroll() {
     let lastScrollTop = 0;
-    const headerHeight = document.querySelector('header').offsetHeight;
+    const header = document.querySelector('header');
+    const headerHeight = header.offsetHeight;
 
     window.addEventListener('scroll', () => {
       let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
       if (currentScroll > lastScrollTop) {
-        document.querySelector('header').style.top = `-${headerHeight}px`; // 숨김
+        header.style.top = `-${headerHeight}px`;
       } else {
-        document.querySelector('header').style.top = "0"; // 표시
+        header.style.top = '0';
       }
 
       lastScrollTop = currentScroll;
@@ -96,8 +70,5 @@ class HeaderManager {
   }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  new HeaderManager();
-});
-
+new HeaderManager();
 export default HeaderManager;
